0f579fdb99ea24b6da91725ee3498dfce88c4cd7
KMinhMilay <plkminh.c918@gmail.com>
2023-05-04 09:03:59 +0700

    cxc

--- NMCNPM_QLNS/Login.Designer.cs
+++ NMCNPM_QLNS/Login.Designer.cs
@@ -173,7 +173,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(301, 26);
             this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "Tên đăng nhập";
+            this.textBox2.Text = "Tên tài khoản";
             this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
             this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
             // 
